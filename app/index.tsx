2320247abf50a0c04f93c603e5d7eb6f16417fb7
Bhushan1019 <patilbhushan1019@gmail.com>
2024-09-20 19:59:25 +0530

    added styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -13,3 +13,62 @@ export default function Index() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    gap: 40,
+    paddingHorizontal: 50,
+  },
+  header: {
+    alignItems: "center",
+    gap: 10,
+  },
+  title: {
+    fontSize: 40,
+    fontFamily: "FrankRuhlLibre_800ExtraBold",
+  },
+  text: {
+    fontSize: 26,
+    textAlign: "center",
+    fontFamily: "FrankRuhlLibre_500Medium",
+  },
+  menu: {
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  btn: {
+    justifyContent: "center",
+    borderRadius: 30,
+    alignItems: "center",
+    borderColor: "#000",
+    borderWidth: 1,
+    width: "60%",
+    maxWidth: 200,
+  },
+  btnText: {
+    padding: 14,
+    fontSize: 16,
+    fontWeight: "semibold",
+    color: "#333",
+  },
+  primaryItem: {
+    backgroundColor: "#000",
+  },
+  primaryText: {
+    color: "#fff",
+  },
+  footer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  footerDate: {
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  footerText: {
+    fontSize: 14,
+  },
+});
