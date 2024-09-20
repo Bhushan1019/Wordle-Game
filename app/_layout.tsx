87e756c80f1c325fb13f53d9887bfea8b883b887
Bhushan1019 <patilbhushan1019@gmail.com>
2024-09-20 22:35:01 +0530

    added dark mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,53 @@
-import { Stack } from "expo-router";
+import { Stack, useRouter } from "expo-router";
+import {
+  useFonts,
+  FrankRuhlLibre_800ExtraBold,
+  FrankRuhlLibre_500Medium,
+  FrankRuhlLibre_900Black,
+} from "@expo-google-fonts/frank-ruhl-libre";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+import { Appearance, Platform, useColorScheme } from "react-native";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const router = useRouter();
+  // const [dark] = useMMKVBoolean('dark-mode', storage);
+
+  // useEffect(() => {
+  //   if (Platform.OS !== 'web') {
+  //     Appearance.setColorScheme(dark ? 'dark' : 'light');
+  //   }
+  // }, [dark]);
+
+  let [fontsLoaded] = useFonts({
+    FrankRuhlLibre_800ExtraBold,
+    FrankRuhlLibre_500Medium,
+    FrankRuhlLibre_900Black,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
   );
 }
